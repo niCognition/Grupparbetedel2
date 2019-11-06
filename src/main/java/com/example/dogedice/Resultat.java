a309a8cde2a4f40823eae3f20882b0e4d236db90
Karin <55209058+kodkarin@users.noreply.github.com>
2019-11-06 10:17:34 +0100

    Ny klass Tarningar

--- src/main/java/com/example/dogedice/Resultat.java
+++ src/main/java/com/example/dogedice/Resultat.java
@@ -1,4 +1,41 @@
 package com.example.dogedice;
 
 public class Resultat {
+
+  public int visaPoang(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalEttor(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalTvaor(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalTreor(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalFyror(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalFemmor(Tarningar tarningar){
+
+    return 0;
+  }
+
+  public int antalSexor(Tarningar tarningar){
+
+    return 0;
+  }
+
+
 }
---
+++ src/main/java/com/example/dogedice/Tarningar.java
@@ -0,0 +1,18 @@
+package com.example.dogedice;
+
+import java.util.ArrayList;
+
+public class Tarningar {
+
+  private ArrayList<Tarning> tarningar;
+
+  public Tarningar(){
+    tarningar = new ArrayList<Tarning>();
+  }
+
+  private void sort(){
+
+  }
+
+
+}
